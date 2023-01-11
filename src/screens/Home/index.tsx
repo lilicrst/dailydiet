de26b3cd54728382c9c017d13a67836c24e3520b
lilicrst <lisandra.crisilva37@gmail.com>
2023-01-11 16:57:11 -0300

    feat: styled components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Container, Title } from './styles';
 
 export function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Daily Diet</Text>
+    <Container>
+      <Title>Daily Diet</Title>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
