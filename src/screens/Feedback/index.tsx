1df23e1b2a0ab0821a6e2030b9edf4fce1e502d1
lilicrst <lisandra.crisilva37@gmail.com>
2023-02-05 22:51:16 -0300

    chore: navigation

--- src/screens/Feedback/index.tsx
+++ src/screens/Feedback/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { ScreenStatusStyleProps, Container, Ilustration, Subtitle, Title } from "./styles";
 
 import { Button } from "@components/Button";
@@ -9,7 +10,12 @@ type Props = {
   status: ScreenStatusStyleProps;
 }
 
-export function Feedback({ status }: Props){
+export function Feedback(){
+
+  const route = useRoute();
+  const { status } = route.params as Props;
+
+  const navigation = useNavigation();
 
   function TitleByStatus({status}: Props){
     if (status === true ) {
@@ -25,6 +31,10 @@ export function Feedback({ status }: Props){
     } else {
       return ("Você saiu da dieta dessa vez, mas continue se esforçando e não desista!")
     }
+  }
+
+  function GoToHome(){
+    navigation.navigate('home');
   }
 
   return (
@@ -43,6 +53,7 @@ export function Feedback({ status }: Props){
       <Button 
         title="Ir para a página inicial"
         type="SMALLER"
+        onPress={GoToHome}
       />
     </Container>
   )
