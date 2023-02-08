1b513839bbff04040329fdeea2c3ab43e4280c7d
lilicrst <lisandra.crisilva37@gmail.com>
2023-02-08 16:21:09 -0300

    chore: Async Storage

---
+++ src/storage/Meal/mealGetAll.ts
@@ -0,0 +1,18 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { MEALS_COLLECTION } from "@storage/storageConfig";
+
+export async function mealGetAll(){
+
+  try {
+
+    const storage = await AsyncStorage.getItem(MEALS_COLLECTION);
+
+    const meals: string[] = storage ? JSON.parse(storage) : [];
+  
+    return meals;
+    
+  } catch (error) {
+    throw error;
+  }
+}
